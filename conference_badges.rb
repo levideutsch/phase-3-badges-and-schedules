24a54eab2a03deff66ab8098eba59e37281e58d3
levideutsch <levideutsch@live.com>
2023-02-22 14:00:15 -0500

    commit tree c5d13e2d0e57c3b9f72949d45c702d2c5470d223

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require 'pry'
+def badge_maker(name) 
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array) 
+    array.map do |item|
+    "Hello, my name is #{item}."
+    end
+end 
+
+def assign_rooms(speakers)
+    speakers.map.with_index do |speaker, index|
+        "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+        # binding.pry
+    end 
+end 
+
+def printer(array) 
+    batch_badge_creator(array).each do |item|
+        puts item
+    end
+    assign_rooms(array).each do |item|
+        puts item 
+    end
+end
